3203e58ee58be8546f45141fa660ccffb6030a64
nikhil suri <nikhil.suri@myntra.com>
2019-01-19 18:22:58 +0530

    main activuty to video activity

--- app/src/main/java/com/example/android/ytlist/MainActivity.java
+++ app/src/main/java/com/example/android/ytlist/MainActivity.java
@@ -1,33 +1,170 @@
 package com.example.android.ytlist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import java.util.Vector;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    RecyclerView recyclerView;
-    Vector<YouTubeVideos> youtubeVideos = new Vector<YouTubeVideos>();
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    //keep track of camera capture intent
+    final int CAMERA_CAPTURE = 1;
+    //captured picture uri
+    private Uri picUri;
+    private Uri cropUri;
+    //keep track of cropping intent
+    final int PIC_CROP = 2;
+    //edittext for getting the tags input
+    //EditText editTextTags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager( new LinearLayoutManager(this));
-
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/eWEF1Zrmdow\" frameborder=\"0\" allowfullscreen></iframe>") );
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/KyJ71G2UxTQ\" frameborder=\"0\" allowfullscreen></iframe>") );
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/y8Rr39jKFKU\" frameborder=\"0\" allowfullscreen></iframe>") );
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/8Hg1tqIwIfI\" frameborder=\"0\" allowfullscreen></iframe>") );
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/uhQ7mh_o_cM\" frameborder=\"0\" allowfullscreen></iframe>") );
-
-        VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos);
-
-        recyclerView.setAdapter(videoAdapter);
+        //retrieve a reference to the UI button
+        Button captureBtn = (Button)findViewById(R.id.capture_btn);
+        //handle button clicks
+        captureBtn.setOnClickListener(this);
     }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.capture_btn) {
+            try {
+                //use standard intent to capture an image
+                Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "bookImage");
+                picUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+                captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, picUri);
+                //we will handle the returned data in onActivityResult
+                startActivityForResult(captureIntent, CAMERA_CAPTURE);
+            } catch (ActivityNotFoundException anfe) {
+                //display an error message
+                String errorMessage = "Whoops - your device doesn't support capturing images!";
+                Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            //user is returning from capturing an image using the camera
+            if(requestCode == CAMERA_CAPTURE){
+                //get the Uri for the captured image
+                //picUri = data.getData();
+                //carry out the crop operation
+                performCrop();
+            } //user is returning from cropping the image
+            else if(requestCode == PIC_CROP){
+                //get the returned data
+                Bundle extras = data.getExtras();
+                Uri croppedUri = data.getData();
+                Bitmap thePic = decodeUriAsBitmap(croppedUri);
+                //get the cropped bitmap
+                //Bitmap thePic = extras.getParcelable("data");//retrieve a reference to the ImageView
+                ImageView picView = (ImageView)findViewById(R.id.picture);
+                //display the returned cropped image
+                picView.setImageBitmap(thePic);
+                //uploadBitmap(thePic);
+                Intent menuIntent = new Intent(this, VideoActivity.class);
+                startActivity(menuIntent);
+
+            }
+        }
+    }
+
+    private Bitmap decodeUriAsBitmap(Uri uri){
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return bitmap;
+    }
+
+    private void performCrop(){
+        try {
+            //call the standard crop action intent (the user device may not support it)
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            //indicate image type and Uri
+            cropIntent.setDataAndType(picUri, "image/*");
+            //set crop properties
+            cropIntent.putExtra("crop", "true");
+            //indicate aspect of desired crop
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            //indicate output X and Y
+            cropIntent.putExtra("outputX", 256);
+            cropIntent.putExtra("outputY", 256);
+            //retrieve data on return
+            cropIntent.putExtra("return-data", true);
+
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, "cropped");
+            picUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, picUri);
+            //start the activity - we handle returning in onActivityResult
+            startActivityForResult(cropIntent, PIC_CROP);
+        }
+        catch(ActivityNotFoundException anfe){
+            //display an error message
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (picUri != null) {
+            outState.putString("cameraImageUri", picUri.toString());
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.containsKey("cameraImageUri")) {
+            picUri = Uri.parse(savedInstanceState.getString("cameraImageUri"));
+        }
+    }
+
+    /*
+     * The method is taking Bitmap as an argument
+     * then it will return the byte[] array for the given bitmap
+     * and we will send this array to the server
+     * here we are using PNG Compression with 80% quality
+     * you can give quality between 0 to 100
+     * 0 means worse quality
+     * 100 means best quality
+     * */
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
 }
